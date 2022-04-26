2f3db8383ddfaced3be006efd19bd5410b7f9ac6
Danila <danila.shulerman123456789@yandex.ru>
2022-04-26 21:17:14 +0300

    выводит массив в консоль

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
-function App() {
-  return (
-    <div>
+const App = () => {
+    let [data, setData] = useState([])
 
-    </div>
-  );
+    const fetchData = () => {
+        fetch("1(2).json")
+            .then(resp => {
+                return resp.json()
+            })
+            .then(data => {
+                setData(data)
+            })
+    }
+    useEffect(() => {
+        fetchData()
+    }, [])
+   // console.log(data)
+    return (
+        <div>
+                {data.map(row => (
+                    data={row}
+                ))}
+        </div>
+    );
 }
 
 export default App;
